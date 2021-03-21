179fce52d4b4c6bd2dafbe8ef9d813048fd7984c
Alex <alex.buzov@icloud.com>
2021-03-21 10:36:34 -0700

    read type

--- Sources/Binary/BinaryReader.swift
+++ Sources/Binary/BinaryReader.swift
@@ -8,12 +8,23 @@ public struct BinaryReader {
         self.bytes = bytes
         self.cursor = cursor
     }
+    
+    public func withCursor(offset: Int) -> Self {
+        BinaryReader(bytes: self.bytes, cursor: cursor + offset)
+    }
 }
 
 public extension BinaryReader {
     func readBit() -> Optional<(Bit, BinaryReader)> {
-        if let bit = bytes.bit(cursor) {
-            return (bit, BinaryReader(bytes: self.bytes, cursor: cursor + 1))
+        bytes.bit(cursor).flatMap { ($0, self.withCursor(offset: 1)) }
+    }
+    
+    func read<T>(_ type: T.Type = T.self, size: Array<UInt8>.Size = MemoryLayout<T>.size * .byte) -> Optional<(T, BinaryReader)> {
+        if let data = try? bytes.bytes(bitRange: cursor...cursor + size - 1) {
+            let typeRead = withUnsafeBytes(of: Data(data).prefix(size / .byte)) {
+                $0.baseAddress!.assumingMemoryBound(to: T.self).pointee
+            }
+            return (typeRead, self.withCursor(offset: cursor + size))
         } else {
             return nil
         }
